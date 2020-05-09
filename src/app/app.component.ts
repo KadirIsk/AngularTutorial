123656b2af7928d39199d8daf238c055b48e51d5
kadir.isik <kadir.isik@mebitech.com>
2020-05-09 13:33:06 +0300

    dishdetail component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'main-component',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+export class Dish {
+    id: string;
+    name: string;
+    imageUrl: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+}
